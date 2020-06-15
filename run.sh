2c76eff0da849ba090f6cbae6e7209a9262c93ea
Mahmudulla Hassan <mhassan@miners.utep.edu>
2020-06-14 23:01:13 -0600

    Update loss function

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # train
-python -m smi_transformer.train -e 1 -v data/vocab.pkl -d data/250k_rndm_zinc_drugs_clean_3.csv -o output -n custom -b 1024 -w 8
+python -m smi_transformer.train -e 200 -v data/vocab.pkl -d data/250k_rndm_zinc_drugs_clean_3.csv -o output -n custom -b 512 -w 40
 
--- smi_transformer/models.py
+++ smi_transformer/models.py
@@ -19,7 +19,7 @@ class PredictorModel(nn.Module):
         self.linear4 = nn.Linear(64, 16)
         self.bn2 = nn.BatchNorm1d(16)
         self.linear5 = nn.Linear(16, out_size)
-        
+
     def forward(self, x):
         x = F.relu(self.linear1(x))
         x = self.dropout1(x)
@@ -28,28 +28,30 @@ class PredictorModel(nn.Module):
         x = F.relu(self.bn1(self.linear3(x)))
         x = F.relu(self.bn2(self.linear4(x)))
         x = self.linear5(x)
-        
+
         return x
-        
+
 
 class PositionalEncoding(nn.Module):
     "Implement the PE function. No batch support?"
+
     def __init__(self, d_model, dropout, max_len=5000):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
-        
+
         # Compute the positional encodings once in log space.
-        pe = torch.zeros(max_len, d_model) # (T,H)
-        position = torch.arange(0., max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0., d_model, 2) * -(math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, d_model)  # (T,H)
+        position = torch.arange(0.0, max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0.0, d_model, 2) * -(math.log(10000.0) / d_model)
+        )
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0)
-        self.register_buffer('pe', pe)
-        
+        self.register_buffer("pe", pe)
+
     def forward(self, x):
-        x = x + Variable(self.pe[:, :x.size(1)], 
-                         requires_grad=False)
+        x = x + Variable(self.pe[:, : x.size(1)], requires_grad=False)
         return self.dropout(x)
 
 
@@ -60,47 +62,63 @@ class TrfmSeq2seq(nn.Module):
         self.hidden_size = hidden_size
         self.embed = nn.Embedding(in_size, hidden_size)
         self.pe = PositionalEncoding(hidden_size, dropout)
-        self.trfm = nn.Transformer(d_model=hidden_size, nhead=4,
-        num_encoder_layers=n_layers, num_decoder_layers=n_layers, dim_feedforward=hidden_size)
+        self.trfm = nn.Transformer(
+            d_model=hidden_size,
+            nhead=4,
+            num_encoder_layers=n_layers,
+            num_decoder_layers=n_layers,
+            dim_feedforward=hidden_size,
+        )
         self.out = nn.Linear(hidden_size, out_size)
 
     def forward(self, src):
         # src: (T,B)
         embedded = self.embed(src)  # (T,B,H)
-        embedded = self.pe(embedded) # (T,B,H)
-        hidden = self.trfm(embedded, embedded) # (T,B,H)
-        out = self.out(hidden) # (T,B,V)
-        out = F.log_softmax(out, dim=2) # (T,B,V)
-        return out # (T,B,V)
+        embedded = self.pe(embedded)  # (T,B,H)
+        hidden = self.trfm(embedded, embedded)  # (T,B,H)
+        out = self.out(hidden)  # (T,B,V)
+        out = F.log_softmax(out, dim=2)  # (T,B,V)
+        return out  # (T,B,V)
 
     def _encode(self, src):
         # src: (T,B)
         embedded = self.embed(src)  # (T,B,H)
-        embedded = self.pe(embedded) # (T,B,H)
+        embedded = self.pe(embedded)  # (T,B,H)
         output = embedded
         for i in range(self.trfm.encoder.num_layers - 1):
             output = self.trfm.encoder.layers[i](output, None)  # (T,B,H)
         penul = output.detach().numpy()
         output = self.trfm.encoder.layers[-1](output, None)  # (T,B,H)
         if self.trfm.encoder.norm:
-            output = self.trfm.encoder.norm(output) # (T,B,H)
+            output = self.trfm.encoder.norm(output)  # (T,B,H)
         output = output.detach().numpy()
         # mean, max, first*2
-        return np.hstack([np.mean(output, axis=0), np.max(output, axis=0), output[0,:,:], penul[0,:,:] ]) # (B,4H)
-    
+        return np.hstack(
+            [
+                np.mean(output, axis=0),
+                np.max(output, axis=0),
+                output[0, :, :],
+                penul[0, :, :],
+            ]
+        )  # (B,4H)
+
     def encode(self, src):
         # src: (T,B)
         batch_size = src.shape[1]
-        if batch_size<=100:
+        if batch_size <= 100:
             return self._encode(src)
-        else: # Batch is too large to load
-            print('There are {:d} molecules. It will take a little time.'.format(batch_size))
-            st,ed = 0,100
-            out = self._encode(src[:,st:ed]) # (B,4H)
-            while ed<batch_size:
+        else:  # Batch is too large to load
+            print(
+                "There are {:d} molecules. It will take a little time.".format(
+                    batch_size
+                )
+            )
+            st, ed = 0, 100
+            out = self._encode(src[:, st:ed])  # (B,4H)
+            while ed < batch_size:
                 st += 100
                 ed += 100
-                out = np.concatenate([out, self._encode(src[:,st:ed])], axis=0)
+                out = np.concatenate([out, self._encode(src[:, st:ed])], axis=0)
             return out
 
 
@@ -111,53 +129,68 @@ class TrfmSeq2seqProp(nn.Module):
         self.hidden_size = hidden_size
         self.embed = nn.Embedding(in_size, hidden_size)
         self.pe = PositionalEncoding(hidden_size, dropout)
-        self.trfm = nn.Transformer(d_model=hidden_size, nhead=4,
-                                   num_encoder_layers=n_layers, num_decoder_layers=n_layers,
-                                   dim_feedforward=hidden_size)
-        self.predict = PredictorModel(220*hidden_size, 3)
+        self.trfm = nn.Transformer(
+            d_model=hidden_size,
+            nhead=4,
+            num_encoder_layers=n_layers,
+            num_decoder_layers=n_layers,
+            dim_feedforward=hidden_size,
+        )
+        self.predict = PredictorModel(220 * hidden_size, 3)
         self.out = nn.Linear(hidden_size, out_size)
 
     def forward(self, src):
         # src: (T,B)
-        #src = src.transpose(0, 1)
+        # src = src.transpose(0, 1)
         embedded = self.embed(src)  # (T,B,H)
-        embedded = self.pe(embedded) # (T,B,H)
-        hidden = self.trfm(embedded, embedded) # (T,B,H)
-        flatten = torch.flatten(hidden, start_dim=1) # (B, 56320)
-        #flatten = torch.flatten(hidden.transpose(0, 1), start_dim=1) # (B, 56320)
+        embedded = self.pe(embedded)  # (T,B,H)
+        hidden = self.trfm(embedded, embedded)  # (T,B,H)
+        flatten = torch.flatten(hidden, start_dim=1)  # (B, 56320)
+        # flatten = torch.flatten(hidden.transpose(0, 1), start_dim=1) # (B, 56320)
         pred = self.predict(flatten)
-        out = self.out(hidden) # (T,B,V)
-        out = F.log_softmax(out, dim=2) # (T,B,V)
-        return out, pred # (T,B,V)
+        out = self.out(hidden)  # (T,B,V)
+        out = F.log_softmax(out, dim=2)  # (T,B,V)
+        return out, pred  # (T,B,V)
 
     def _encode(self, src):
         # src: (T,B)
         embedded = self.embed(src)  # (T,B,H)
-        embedded = self.pe(embedded) # (T,B,H)
+        embedded = self.pe(embedded)  # (T,B,H)
         output = embedded
         for i in range(self.trfm.encoder.num_layers - 1):
             output = self.trfm.encoder.layers[i](output, None)  # (T,B,H)
         penul = output.detach().numpy()
         output = self.trfm.encoder.layers[-1](output, None)  # (T,B,H)
         if self.trfm.encoder.norm:
-            output = self.trfm.encoder.norm(output) # (T,B,H)
+            output = self.trfm.encoder.norm(output)  # (T,B,H)
         output = output.detach().numpy()
         # mean, max, first*2
-        return np.hstack([np.mean(output, axis=0), np.max(output, axis=0), output[0,:,:], penul[0,:,:] ]) # (B,4H)
-    
+        return np.hstack(
+            [
+                np.mean(output, axis=0),
+                np.max(output, axis=0),
+                output[0, :, :],
+                penul[0, :, :],
+            ]
+        )  # (B,4H)
+
     def encode(self, src):
         # src: (T,B)
         batch_size = src.shape[1]
-        if batch_size<=100:
+        if batch_size <= 100:
             return self._encode(src)
-        else: # Batch is too large to load
-            print('There are {:d} molecules. It will take a little time.'.format(batch_size))
-            st,ed = 0,100
-            out = self._encode(src[:,st:ed]) # (B,4H)
-            while ed<batch_size:
+        else:  # Batch is too large to load
+            print(
+                "There are {:d} molecules. It will take a little time.".format(
+                    batch_size
+                )
+            )
+            st, ed = 0, 100
+            out = self._encode(src[:, st:ed])  # (B,4H)
+            while ed < batch_size:
                 st += 100
                 ed += 100
-                out = np.concatenate([out, self._encode(src[:,st:ed])], axis=0)
+                out = np.concatenate([out, self._encode(src[:, st:ed])], axis=0)
             return out
 
 
@@ -168,17 +201,22 @@ class TrfmSeq2seqProp2(nn.Module):
         self.hidden_size = hidden_size
         self.embed = nn.Embedding(in_size, hidden_size)
         self.pe = PositionalEncoding(hidden_size, dropout)
-        self.trfm = nn.Transformer(d_model=hidden_size, nhead=4,
-                                   num_encoder_layers=n_layers, num_decoder_layers=n_layers,
-                                   dim_feedforward=hidden_size)
-        self.predict = PredictorModel(220*hidden_size, 3)
+        self.trfm = nn.Transformer(
+            d_model=hidden_size,
+            nhead=4,
+            num_encoder_layers=n_layers,
+            num_decoder_layers=n_layers,
+            dim_feedforward=hidden_size,
+        )
+        self.predict = PredictorModel(hidden_size, 3)
+        # self.predict = PredictorModel(220*hidden_size, 3)
         self.out = nn.Linear(hidden_size, out_size)
 
     def forward(self, src):
         # src: (B,T)
-        #src = src.transpose(0, 1)
+        # src = src.transpose(0, 1)
         embedded = self.embed(src)  # (B,T,H)
-        embedded = self.pe(embedded) # (B,T,H)
+        embedded = self.pe(embedded)  # (B,T,H)
         output = embedded
         for mod in self.trfm.encoder.layers:
             output = mod(output, None)
@@ -189,40 +227,52 @@ class TrfmSeq2seqProp2(nn.Module):
             output = mod(output, embedded)
         if self.trfm.decoder.norm:
             output = self.trfm.decoder.norm(output)
-        
-        flatten = torch.flatten(encoded, start_dim=1) # (B, 56320) ##########
-        #flatten = torch.flatten(hidden.transpose(0, 1), start_dim=1) # (B, 56320)
-        pred = self.predict(flatten) # (B,3)
-        out = self.out(output) # (B,T,V)
-        out = F.log_softmax(out, dim=2) # (B,T,V)
-        return out, pred 
+        out = self.out(output)  # (B,T,V)
+        out = F.log_softmax(out, dim=2)  # (B,T,V)
+
+        flatten = encoded.mean(1)
+        # flatten = torch.flatten(encoded, start_dim=1) # (B, 56320) ##########
+        # flatten = torch.flatten(hidden.transpose(0, 1), start_dim=1) # (B, 56320)
+        pred = self.predict(flatten)  # (B,3)
+        return out, pred
 
     def _encode(self, src):
         # src: (B,T)
         embedded = self.embed(src)  # (B,T,H)
-        embedded = self.pe(embedded) # (B,T,H)
+        embedded = self.pe(embedded)  # (B,T,H)
         output = embedded
         for i in range(self.trfm.encoder.num_layers - 1):
             output = self.trfm.encoder.layers[i](output, None)  # (B,T,H)
         penul = output.cpu().detach().numpy()
         output = self.trfm.encoder.layers[-1](output, None)  # (B,T,H)
         if self.trfm.encoder.norm:
-            output = self.trfm.encoder.norm(output) # (B,T,H)
+            output = self.trfm.encoder.norm(output)  # (B,T,H)
         output = output.cpu().detach().numpy()
         # mean, max, first*2
-        return np.hstack([np.mean(output, axis=1), np.max(output, axis=1), output[:,0,:], penul[:,0,:] ]) # (B,4H)
-    
+        return np.hstack(
+            [
+                np.mean(output, axis=1),
+                np.max(output, axis=1),
+                output[:, 0, :],
+                penul[:, 0, :],
+            ]
+        )  # (B,4H)
+
     def encode(self, src):
         # src: (T,B)
         batch_size = src.shape[0]
-        if batch_size<=100:
+        if batch_size <= 100:
             return self._encode(src)
-        else: # Batch is too large to load
-            print('There are {:d} molecules. It will take a little time.'.format(batch_size))
-            st,ed = 0,100
-            out = self._encode(src[:,st:ed]) # (B,4H)
-            while ed<batch_size:
+        else:  # Batch is too large to load
+            print(
+                "There are {:d} molecules. It will take a little time.".format(
+                    batch_size
+                )
+            )
+            st, ed = 0, 100
+            out = self._encode(src[:, st:ed])  # (B,4H)
+            while ed < batch_size:
                 st += 100
                 ed += 100
-                out = np.concatenate([out, self._encode(src[:,st:ed])], axis=0)
+                out = np.concatenate([out, self._encode(src[:, st:ed])], axis=0)
             return out
